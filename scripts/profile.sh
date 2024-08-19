8f4a84bd5b89cb4963ed508a014758fc555c06e5
Jeremy Muhlich <jmuhlich@bitflood.org>
2024-08-19 12:04:09 -0400

    Move profile setup to separate script and add keyring init

---
+++ scripts/profile.sh
@@ -0,0 +1,19 @@
+# Set default directory for shell.
+cat << EOF > ~/.bashrc.d/00-cwd
+test -e "$GITPOD_REPO_ROOT" && cd "$GITPOD_REPO_ROOT"
+EOF
+
+
+# Initialize empty keyring for Chromium to avoid keyring dialog.
+mkdir -p ~/.local/share/keyrings/
+echo -n "Default_keyring" > ~/.local/share/keyrings/default
+cat > ~/.local/share/keyrings/Default_keyring.keyring << EOF
+[keyring]
+display-name=Default keyring
+ctime=0
+mtime=0
+lock-on-idle=false
+lock-after=false
+EOF
+chmod og= ~/.local/share/keyrings/
+chmod og= ~/.local/share/keyrings/Default_keyring.keyring
